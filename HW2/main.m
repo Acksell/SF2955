71b15727041f5f477b67deb1ba4b9188eb741f1c
Daniel Schmekel <daniel.schmekel@gmail.com>
2021-05-18 21:07:30 +0200

    Fixed ndiasters & acceptance rate

--- HW2/main.m
+++ HW2/main.m
@@ -2,20 +2,19 @@
 tau=load("coal-mine.csv");
 
 %%
-
+close all
 % Gibbs sampling + MH
 rng(42)
 
 
 
 params=struct();
-params.d=5;
+params.d=3;
 params.vartheta=2;
+params.tau = tau;
 
 t=linspace(1851,1963, params.d+1);
-
-params.ndisasters=ndisasters(tau,t);
-params.rho=5;
+params.rho=1;
 
 % t=randn(1,d);
 % t=[1851 t 1963];
@@ -24,7 +23,7 @@ params.rho=5;
 lambda=ones(1, params.d);
 theta=(4)/(sum(lambda) + params.vartheta);
 
-K=50000;
+K=1000;
 thetaStore=zeros(K,1);
 lambdaStore=zeros(K,params.d);
 tStore=zeros(K,params.d+1);
@@ -35,17 +34,36 @@ for i=1:K
     tStore(i,:)=t;
 end
 %%
-burnIn=400;
+burnIn= 0.3 * K;
 figure
-loglog(thetaStore(burnIn:end))
+plot(thetaStore(burnIn:end))
+title("Theta")
 figure
-plot(lambdaStore(burnIn:end,:))
+plot(lambdaStore(burnIn:end,1))
+title("Lambdas")
 figure
-loglog(tStore(burnIn:end,:))
+plot(tStore(burnIn:end,:))
+title("Breakpoints")
+figure
+hold on
+hist(params.tau, 30)
+for i = 2:params.d
+  line([t(i), t(i)], ylim, 'LineWidth', 2, 'Color', 'r');
+end
 
 %%
 function [n]=ndisasters(tau, t)
-    n=histcounts(tau,t);
+%   n = zeros(1, length(t) - 1);
+%   prevIdx = 1;
+%   for i = 2:length(t)
+%     newIdx = find(tau > t(i), 1);
+%     if isempty(newIdx)
+%       newIdx = length(t);
+%     end
+%     n(i - 1) = newIdx - prevIdx;
+%     prevIdx = newIdx;
+%   end
+  n=histcounts(tau,t);
 end
 
 function [theta, lambda, t]=GibbsStep(~, lambda, t, params)
@@ -55,14 +73,21 @@ function [theta, lambda, t]=GibbsStep(~, lambda, t, params)
 end
 
 function [theta]=thetaConditional(lambda, params)
-    theta=gamrnd(4, 1/(sum(lambda) + params.vartheta));
+    theta= 4/(sum(lambda) + params.vartheta);%gamrnd(4, 1/(sum(lambda) + params.vartheta));
 end
 
 function [lambda]=lambdaConditional(theta, t, params)
     lambda=zeros(params.d, 1);
+    nDisasters = ndisasters(params.tau, t);
     for i=1:length(lambda)
-        lambda(i)=gamrnd(params.ndisasters(i)+2, 1/(t(i+1)-t(i)+theta));
+        lambda(i)= (nDisasters(i) + 2) / (t(i + 1) - t(i) + theta);%gamrnd(nDisasters(i)+2, 1/(t(i+1)-t(i) + theta));
     end
+end
+
+function [p] = tConditional(t, lambdas, params)
+    nDisasters = ndisasters(params.tau, t);
+    T = diff(t);
+    p = exp(-T .* lambdas') * prod(lambdas' .^ nDisasters) * prod(T);
 end
 
 function [t]=tMetropolisHastings(lambda, t, params)
@@ -76,9 +101,17 @@ function [t]=tMetropolisHastings(lambda, t, params)
 %         size(t(1:end-2))
         tcand(2:end-1)=t(1:end-2) + epsilon.*(t(3:end) - t(1:end-2));
 
-        U=rand(1,params.d);
+        U = rand(1,params.d);
         for i=2:(params.d)
-            alpha=exp(lambda(i)*(tcand(i) - t(i))-lambda(i-1)*(tcand(i) - t(i)))*(t(i+1)-tcand(i))/(tcand(i)-t(i-1));
+            if tcand(i) > tcand(i + 1)
+              alpha = 0;
+            else
+              %alpha=exp(lambda(i)*(tcand(i) - t(i))-lambda(i-1)*(tcand(i) - t(i)))*(t(i+1)-tcand(i))/(tcand(i)-t(i-1));
+              tmp = t;
+              tmp(i) = tcand(i);
+              alpha = tConditional(tmp, lambda, params) / tConditional(t, lambda, params);
+            end
+                         
             if U(i-1)<=alpha
                 t(i)=tcand(i);
             end
